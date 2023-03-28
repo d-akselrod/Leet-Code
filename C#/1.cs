788aa93fe7d2be04189373040df635bedb914e65
Daniel Akselrod <dakselrod@outlook.com>
2023-03-28 19:55:16 -0400

    1. Two Sum

---
+++ C#/1.cs
@@ -0,0 +1,18 @@
+public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        for (int i = 0; i < nums.Length; i ++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[2]{i,j};
+                }
+            }
+        }
+
+        return new int[2]{0,0};
+    }
+}
